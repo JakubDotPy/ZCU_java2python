22b71ff4e15311c4f8004b2178ab8223ad6329e6
Jakub Červinka <cervinka.jakub.1989@gmail.com>
2019-01-23 21:44:34 +0100

    Solved cv02

---
+++ PPA1/cv02/PPA1_cv02_pythonic.py
@@ -0,0 +1,22 @@
+# solution for PPA1 cv02 exercise
+
+import turtle 
+
+scr = turtle.Screen() 
+scr.bgcolor("yellow")
+
+t = turtle.Turtle()
+t.speed(0)
+t.setpos(0,0)
+
+t.color('blue')
+t.fd(100)
+t.lt(120)
+
+t.color('red')
+t.fd(100)
+t.lt(120)
+
+t.color('green')
+t.fd(100)
+t.lt(120)
